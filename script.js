314e35865f8aa2bac6f3954d916d5d7adbd6d592
swastik sharma <swastiksharma9010@gmail.com>
2026-06-20 22:48:26 +0530

    numbers of vowel in a string using function

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
-const arrowmod = (a,b)=>{
-    console.log(a%b);
-};
+function myVowels(str){
+    let count=0;
+    for(const char of str)
+    {
+        if(
+            char ==='a'||
+            char ==='e'||
+        char ==='i'||
+char ==='o'||
+char ==='u'||
+char ==='A'||
+char ==='E'||
+char ==='I'||
+char ==='O'||
+char ==='U'
+        )
+        {
+            count++;
+        }
+        }
+        console.log(count);
+}
+myVowels("HelloAA");
