b759bb58eaf355017d07713915b12c9ed6981148
swastik sharma <swastiksharma9010@gmail.com>
2026-06-24 17:31:11 +0530

    using add event listener in js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let box=document.querySelector("#box");
-box.onmouseover=(e)=>{
-console.log(e);
-};
+let btn1 = document.querySelector("#btn1");
+btn1.addEventListener("click", ()=>{
+    console.log("button was clicked");
+})
