fe3192c19f11185892d90cd0e107a845c427089f
swastik sharma <swastiksharma9010@gmail.com>
2026-06-25 15:42:08 +0530

    added changing color to page by clicking button(dark/light)

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ let mode="light";
 btn1.addEventListener("click",()=>{
     if(mode==="light"){
         mode="dark";
+        document.querySelector("body").style.backgroundColor="black";
     }else{
         mode="light";
+        document.querySelector("body").style.backgroundColor="white";
     }
     console.log(mode);
 });
