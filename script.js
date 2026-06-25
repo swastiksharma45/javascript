11503fd51e9ce23c5f156801e61d1d296eb301a8
swastik sharma <swastiksharma9010@gmail.com>
2026-06-25 17:02:19 +0530

    using css and js for dark and light modre toggle

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 let btn1=document.querySelector("#btn1");
+let body=document.querySelector("body");
 let mode="light";
 btn1.addEventListener("click",()=>{
     if(mode==="light"){
         mode="dark";
-        document.querySelector("body").style.backgroundColor="black";
+        body.classList.add("dark")
+        body.classList.remove("light");
     }else{
         mode="light";
-        document.querySelector("body").style.backgroundColor="white";
+        body.classList.add("light");
+        body.classList.remove("dark");
     }
     console.log(mode);
 });
